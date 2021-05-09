d80b8420a56aa2bb8c732aa78f044d6ffdb87bbe
neilnikkunilesh <nileshgupta1993@gmail.com>
2021-05-09 22:15:53 +0530

    initial Commit

---
+++ src/context.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
+    case 'ADD_CONTACT':
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts],
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Dow',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555',
+      },
+      {
+        id: 2,
+        name: 'Karen Williams',
+        email: 'karenw@gmail.com',
+        phone: '888-999-0000',
+      },
+      {
+        id: 3,
+        name: 'Henry Johnson',
+        email: 'johnh@gmail.com',
+        phone: '111-2222-222',
+      },
+    ],
+    dispatch: (action) => this.setState((state) => reducer(state, action)),
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;

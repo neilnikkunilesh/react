74e02bcdc63ac2f65fc5dca5587c5cd8ea60fcc7
neilnikkunilesh <nileshgupta1993@gmail.com>
2021-05-16 16:23:59 +0530

    Added the Edit contact functionality

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import { Consumer } from '../../context';
 import axios from 'axios';
@@ -44,6 +45,15 @@ class Contact extends Component {
                   style={{ cursor: 'pointer', float: 'right', color: 'red' }}
                   onClick={this.onDeleteClick.bind(this, id, dispatch)}
                 ></i>
+                <Link to={`/contact/edit/${id}`}>
+                  <i className="fas fa-pencil-alt"
+                  style={{
+                    cursor: 'pointer',
+                    float: 'right',
+                    color: 'black',
+                    marginRight: '1rem'
+                  }}></i>
+                </Link>
               </h4>
               {showContactInfo ? (
                 <ul className="list-group">
